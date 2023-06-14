6d1606afb255ccd789c82e342ff95e34d023943b
owenschock1 345 <owenpschock@gmail.com>
2023-06-14 16:39:43 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+     applyFilter()
   
 
   // do not change the below line of code
@@ -32,7 +32,17 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter() {
+  for (var i = 0; r < image.length; i++) {
+    for (var j = 0; j < image.length; c++) {
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      rgbNumbers[RED] = 255;
+      rgbString = rgbArrayToString(rgbNumbers);
+      rgbString = image[i][j];
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
 
